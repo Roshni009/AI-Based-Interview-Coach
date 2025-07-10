0feff3350a7a40e2c3432ba000a4c31373fd5b3e
Roshni Kumari <144609293+Roshni009@users.noreply.github.com>
2025-07-10 09:01:22 +0530

    after some changes

---
+++ app/(main)/onboarding/page.jsx
@@ -0,0 +1,25 @@
+
+import { industries } from '@/data/industries';
+import { redirect } from 'next/navigation';
+import React from 'react'
+import OnboardingForm from './_components/onboarding-form';
+import getUserOnboardingStatus from '@/actions/user';
+
+
+const OnboardingPage =  async () => {
+    // check if user is already onboarded
+
+    const { isOnboarded } = await getUserOnboardingStatus();
+
+    if(isOnboarded) {
+        redirect("/dashboard");
+    }
+
+  return (
+    <main>
+        <OnboardingForm industries={industries}/>
+    </main>
+  )
+}
+
+export default OnboardingPage;
